31bb4244db7352918f56db944fab8003274f11e7
Merctron <smartomot@hotmail.com>
2015-02-28 13:25:29 -0600

    Server ready

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var http = require('http');
 var url = require('url');
-
+var parse = require('parse');
+//parse.initialize("mSjyVccMyMBh8fxcY6zkIYlDEXiXgwjngbKoiFM8", "IA5wobRXq295jg2YucJ7TavgM8pD4848K2FlOknj");
 var qs = require('querystring');
 var request = require('request');
 
 var server = http.createServer( function(req, res) {
 
-    
+    var answers = [];
+    var sessions = [];
+
+    var sessionExists = function(sess) {
+      for (i = 0; i < sessions.length; i++) {
+        if (sessions[i] == sess) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    var sessionIndex = function(sess) {
+      for (i = 0; i < sessions.length; i++) {
+        if (sessions[i] == sess) {
+          return i;
+        }
+      }
+      return "no session found";
+    };
+
     res.writeHead(200, {
         'content-type': 'application/json;charset=utf-8',
         'Access-Control-Allow-Origin': '*',
@@ -30,27 +51,61 @@ var server = http.createServer( function(req, res) {
 
     req.on('end', function () {
       var post = qs.parse(body);
-            
-      console.log(post);
-      resp.consoleLog = post;
-      console.log(req.method);
-      resp.success = true;  
-      resp.answer = "a";   
-      res.end(JSON.stringify(resp));
+
+
+      if (post.requestType == "webApp") {
+        if (sessionExists(post.session)) {
+          console.log(post);
+          resp.consoleLog = post;
+          console.log(req.method);
+          resp.success = true;
+          resp.vals = answers;    
+          res.end(JSON.stringify(resp));
+          answers = [];
+        }
+        else {
+          sessions.push(post.session);
+          answers.push([]);
+          console.log(post);
+          resp.consoleLog = post;
+          console.log(req.method);
+          resp.success = true;     
+          res.end(JSON.stringify(resp));
+        }
+      }
+      else if (post.requestType == "client") {
+        if (sessionExists(post.session)) {
+          answers[sessionIndex(post.session)].push(post.answer);
+  
+          console.log(post);
+          resp.consoleLog = post;
+          console.log(req.method);
+          resp.success = true;     
+          res.end(JSON.stringify(resp));
+        }
+        else {
+          console.log(post);
+          resp.consoleLog = post;
+          console.log(req.method);
+          resp.success = false;     
+          res.end(JSON.stringify(resp));
+        }
+      }
+
     });
 });
 
 server.listen(8080);
 
 
-var io = require('socket.io').listen(server);
-//creating a new websocket to keep the content updated without any AJAX request
-io.sockets.on('connection', function(socket) {
-  var json = {
-    "answer": "a"
-  };
+// var io = require('socket.io').listen(server);
+// //creating a new websocket to keep the content updated without any AJAX request
+// io.sockets.on('connection', function(socket) {
+//   var json = {
+//     "answer": "a"
+//   };
   
-  socket.volatile.emit('notification', json);
-});
+//   socket.volatile.emit('notification', json);
+// });
 
 
