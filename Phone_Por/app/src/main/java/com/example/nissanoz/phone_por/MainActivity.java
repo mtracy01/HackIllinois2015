bfbb6fe72e17aab22f11819ff893e9849dba0246
Omar <opineda@purdue.edu>
2015-02-28 13:00:32 -0500

    Changing simple stuff

--- Phone_Por/app/src/main/java/com/example/nissanoz/phone_por/MainActivity.java
+++ Phone_Por/app/src/main/java/com/example/nissanoz/phone_por/MainActivity.java
@@ -4,14 +4,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Initialize()
     }
 
 
@@ -35,5 +39,43 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void Initialize()
+    {
+        TextView txt = (TextView) findViewById(R.id.Session);
+        TextView txt2 = (TextView) findViewByID(R.id.Exit);
+        TextView txt3 = (TextView) findViewById(R.id.AU);
+        Button connect = (Button) findViewById(R.id.bConnect);
+        Button leave = (Button) findViewById(R.id.bExit);
+        Button about = (Button) findViewById(R.id.bGo);
+        connect.setOnClickListener(this);
+        leave.setOnClickListener(this);
+        about.setOnClickListener(this);
+    }
+
+    public void onBackPressed()
+    {
+
+    }
+
+    public void backButtonHandler()
+    {
+
+    }
+
+    public void onClick(View v)
+    {
+     switch(v.getId())
+     {
+         case R.id.bConnect:
+             break;
+
+         case R.id.bExit:
+             break;
+
+         case R.id.bGo:
+             break;
+     }
     }
 }
